bc3c7437fabd6b759db65bbe66002635ec531f3f
Alexander Obozny <alexander.obozny@gmail.com>
2014-03-16 10:49:01 +0200

    Algorithm doesn't stuck in endless cycle anymore.

--- src/util/ZongSyn.java
+++ src/util/ZongSyn.java
@@ -1,15 +1,74 @@
 package util;
 
 import java.awt.image.WritableRaster;
-import java.util.Arrays;
 
 public class ZongSyn {
+
+    public static WritableRaster skeletonizaciya(int[][] matrix, WritableRaster binarizedData) {
+        boolean isPixelRemoved;
+        int[][] parallelMatrix = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            parallelMatrix[i] = matrix[i].clone();
+        }
+        do {
+            isPixelRemoved = false;
+            for (int y = 1; y < matrix.length - 1; y++) {
+                for (int x = 1; x < matrix[y].length - 1; x++) {
+                    if (matrix[y][x] == 0) {
+                        continue;
+                    }
+                    if (isPointBelongsToContour(matrix, x, y)) {
+                        boolean c1 = condition1(matrix, x, y);
+                        boolean c2 = condition2(matrix, x, y);
+                        boolean c3a = condition3a(matrix, x, y);
+                        boolean c4a = condition4a(matrix, x, y);
+                        if (c1 && c2 && c3a && c4a) {
+                            parallelMatrix[y][x] = 0;
+                            isPixelRemoved = true;
+                        }
+                    }
+                }
+            }
+            for (int y = 0; y < matrix.length; y++) {
+                for (int x = 0; x < matrix[y].length; x++) {
+                    if (matrix[y][x] == 0) {
+                        continue;
+                    }
+                    if (isPointBelongsToContour(matrix, x, y)) {
+                        boolean c1 = condition1(matrix, x, y);
+                        boolean c2 = condition2(matrix, x, y);
+                        boolean c3b = condition3b(matrix, x, y);
+                        boolean c4b = condition4b(matrix, x, y);
+                        if (c1 && c2 && c3b && c4b) {
+                            parallelMatrix[y][x] = 0;
+                            isPixelRemoved = true;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < parallelMatrix.length; i++) {
+                matrix[i] = parallelMatrix[i].clone();
+            }
+        } while (isPixelRemoved);
+        int[] blackPixel = {0, 0, 0};
+        int[] whitePixel = {255, 255, 255};
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 0) {
+                    binarizedData.setPixel(x, y, whitePixel);
+                } else {
+                    binarizedData.setPixel(x, y, blackPixel);
+                }
+            }
+        }
+        return binarizedData;
+    }
+
     /*
      P9	P2 P3
      P8	P1 P4
      P7	P6 P5
      */
-
     //2 <= P2+P3+...+P8+P9 <= 6
     private static boolean condition1(int[][] matrix, int x, int y) {
         int p2 = matrix[y - 1][x];
@@ -118,76 +177,23 @@ public class ZongSyn {
         return result;
     }
 
-    public static WritableRaster skeletonizaciya(int[][] matrix, WritableRaster binarizedData) {
-        //Display matrix
-        for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
-        }
-        boolean isPixelRemoved;
-        int iterationNumber = 0;
-        int[][] parallelMatrix = new int[matrix.length][];
-        for (int i = 0; i < matrix.length; i++) {
-            parallelMatrix[i] = matrix[i].clone();
-        }
-        do {
-            isPixelRemoved = false;
-            for (int y = 0; y < matrix.length; y++) {
-                for (int x = 0; x < matrix[y].length; x++) {
-                    boolean c1 = false;
-                    boolean c2 = false;
-                    boolean c3a = false;
-                    boolean c4a = false;
-                    try {
-                        c1 = condition1(matrix, x, y);
-                        c2 = condition2(matrix, x, y);
-                        c3a = condition3a(matrix, x, y);
-                        c4a = condition4a(matrix, x, y);
-                    } catch (IndexOutOfBoundsException e) {
-                    }
-                    if (c1 && c2 && c3a && c4a) {
-                        parallelMatrix[y][x] = 0;
-                        isPixelRemoved = true;
-                    }
-                }
-            }
-            for (int y = 0; y < matrix.length; y++) {
-                for (int x = 0; x < matrix[y].length; x++) {
-                    boolean c1 = false;
-                    boolean c2 = false;
-                    boolean c3b = false;
-                    boolean c4b = false;
-                    try {
-                        c1 = condition1(matrix, x, y);
-                        c2 = condition2(matrix, x, y);
-                        c3b = condition3b(matrix, x, y);
-                        c4b = condition4b(matrix, x, y);
-                    } catch (IndexOutOfBoundsException e) {
-                    }
-                    if (c1 && c2 && c3b && c4b) {
-                        parallelMatrix[y][x] = 0;
-                        isPixelRemoved = true;
-                    }
-                }
-            }
-            for (int i = 0; i < parallelMatrix.length; i++) {
-                matrix[i] = parallelMatrix[i].clone();
-            }
-//            System.out.println("Iteration " + iterationNumber++ + " end. Pixel removed = " + isPixelRemoved);
-        } while (isPixelRemoved);
-        for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
-        }
-        int[] blackPixel = {0, 0, 0};
-        int[] whitePixel = {255, 255, 255};
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                if (matrix[y][x] == 0) {
-                    binarizedData.setPixel(x, y, whitePixel);
-                } else {
-                    binarizedData.setPixel(x, y, blackPixel);
-                }
-            }
-        }
-        return binarizedData;
+    private static boolean isPointBelongsToContour(int matrix[][], int x, int y) {
+        int p2 = matrix[y - 1][x];
+        int p3 = matrix[y - 1][x + 1];
+        int p4 = matrix[y][x + 1];
+        int p5 = matrix[y + 1][x + 1];
+        int p6 = matrix[y + 1][x];
+        int p7 = matrix[y + 1][x - 1];
+        int p8 = matrix[y][x - 1];
+        int p9 = matrix[y - 1][x - 1];
+        boolean result = (p2 == 0)
+                || (p3 == 0)
+                || (p4 == 0)
+                || (p5 == 0)
+                || (p6 == 0)
+                || (p7 == 0)
+                || (p8 == 0)
+                || (p9 == 0);
+        return result;
     }
 }
